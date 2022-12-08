cb3b20f697bb28ceb9a1ad6fd40e798da3719bd3
CamusDeAqua <103545259+CamusDeAqua@users.noreply.github.com>
2022-12-08 04:19:41 -0300

    Update index.js

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export function getInfo(){ 
     return async (dispatch)=>{
-        let json = await axios.get('ps-ch-deploy.herokuapp.com/login')
+        let json = await axios.get('http://localhost:3001/pragmasoft')
         console.log(json.data.series)
         return dispatch({
             type: 'GET_INFO',
@@ -23,7 +23,7 @@ export function postLogin(input){
 }
 export function registerOperation(payload){
     return async () => {
-        let json = await axios.post('ps-ch-deploy.herokuapp.com/login', payload);
+        let json = await axios.post('http://localhost:3001/pragmasoft', payload);
         return json;        
     }
 }
